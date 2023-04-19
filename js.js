a972eab96ad8156de6b4bc962c2ed7b38b1d04d6
pblake31 <120692538+pblake31@users.noreply.github.com>
2023-04-19 16:06:09 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,38 @@
+function publishNumber() {
+  //Create a new Client object with your broker's hostname, port and your own clientId
+  var client = new Paho.MQTT.Client('broker.mqttdashboard.com', 8000, 'clientId');
+
+  var options = {
+  //connection attempt timeout in seconds
+  timeout: 3,
+  
+  onSuccess: function () {
+      //select component dropdown (Topic)
+      var dropdown = document.getElementById("dropdown").value;
+      
+      //add component to SMD topics
+      var topic = 'SmartSMD/Feeder/' + dropdown;
+      
+      //select quantity (Message)
+      var quantity = document.getElementById("quantity-input").value; 
+      var message = new Paho.MQTT.Message(quantity);
+      
+      //publish messsage
+      message.destinationName = topic;
+      message.qos = 2;
+      client.send(message);
+      
+      //Alert Message that sends user back to homepage
+      alert("Connection Successful: Your components will be dispensed from the Smart Shelf. Click OK to return home");
+      window.location.href = "index.html";
+  },
+
+  onFailure: function (message) {
+      console.log("Connection failed: " + message.errorMessage);
+      alert("Connection failed: " + message.errorMessage);
+  }
+};
+
+//Attempt to connect
+client.connect(options);
+};
